e82be0d2016d6e0fd2ea7f80b77e7b2609835997
Viktoriia Cherneha <vi.cherneha@gmail.com>
2021-05-13 12:59:11 +0300

    Back navigation in RegisterActivity.kt

--- app/src/main/java/com/example/chatapp/RegisterActivity.kt
+++ app/src/main/java/com/example/chatapp/RegisterActivity.kt
@@ -3,14 +3,21 @@ package com.example.chatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.example.chatapp.databinding.ActivityMainBinding
+import com.example.chatapp.databinding.ActivityRegisterBinding
 import com.example.chatapp.utilits.REGISTER_ACTIVITY
 import com.example.chatapp.utilits.replaceFragment
+import com.example.chatapp.view.ChatsFragment
 import com.example.chatapp.view.GetStartedFragment
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)       // binding initializing
+        setContentView(binding.root)
 
         REGISTER_ACTIVITY = this
         setStatusBarParams()
@@ -25,5 +32,19 @@ class RegisterActivity : AppCompatActivity() {
         view.setSystemUiVisibility(view.getSystemUiVisibility() or View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR)
         // white background color
         window.statusBarColor = this.resources.getColor(R.color.white)
+    }
+
+    override fun onBackPressed() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.activity_container)
+
+        if (currentFragment != null) {
+            // if current fragment is ChatsFragment app closes
+            if (currentFragment is GetStartedFragment) {
+                super.onBackPressed()
+                finish()
+            } else {
+                super.onBackPressed()
+            }
+        }
     }
 }
